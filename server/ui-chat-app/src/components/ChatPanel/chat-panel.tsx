9f74c5c7cc1e3af36377fa76b2ac941ad473fbe0
Deepesh Tyagi <tyagideepesh3@gmail.com>
2024-01-01 08:52:10 -0800

    minor-changes

--- server/ui-chat-app/src/components/ChatPanel/chat-panel.tsx
+++ server/ui-chat-app/src/components/ChatPanel/chat-panel.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { IChatPanel } from "./interface";
-
+import "./style.css";
 const ChatPanel = (props: IChatPanel) => {
   const { selectedUser, setSelectedUser } = props;
   const [socket, setSocket] = useState<any | null>();
+  const [userText, setUserText] = useState<string | null>();
   useEffect(() => {
     const socket = io("http://localhost:3001");
     setSocket(socket);
@@ -21,10 +22,21 @@ const ChatPanel = (props: IChatPanel) => {
     //   console.log(data);
     // });
   }, [selectedUser]);
+  const ChangeHandler = (e: any) => {
+    // console.log(e.target.value);
+    setUserText(e.target.value);
+  };
+  const SendButtonHandler = () => {
+    // console.log("send ", userText);
+  };
   return (
-    <div>
+    <div className="chat-container">
       <div className="chat-panel-title">
         {selectedUser && <h2>{selectedUser.name.toLocaleUpperCase()}</h2>}
+        <div>
+          <input type="text" onChange={ChangeHandler} />
+          <button onClick={SendButtonHandler}>Send</button>
+        </div>
       </div>
       <div>Chat Section Main</div>
     </div>
